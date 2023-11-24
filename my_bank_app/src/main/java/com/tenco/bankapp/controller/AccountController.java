e9d544c2283454c00ea3936be8d029cca8eb6ce9
muaga <more9494@naver.com>
2023-11-24 14:05:51 +0900

    account save

--- my_bank_app/src/main/java/com/tenco/bankapp/controller/AccountController.java
+++ my_bank_app/src/main/java/com/tenco/bankapp/controller/AccountController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tenco.bankapp.dto.SaveFormDto;
 import com.tenco.bankapp.handler.exception.CustomPageException;
 import com.tenco.bankapp.handler.exception.CustomRestfullException;
 import com.tenco.bankapp.handler.exception.UnAuthrizedException;
 import com.tenco.bankapp.repository.entity.User;
+import com.tenco.bankapp.service.AccountService;
 import com.tenco.bankapp.utils.Define;
 
 @Controller
@@ -21,7 +24,9 @@ public class AccountController {
 	@Autowired
 	private HttpSession session;
 	
-	// 임시 예외 발생 확인
+	@Autowired
+	private AccountService accountService;
+	
 	// http://localhost:80/account/list
 	@GetMapping({"/list", "/"})
 	public String list() {
@@ -32,6 +37,47 @@ public class AccountController {
 		if(principal == null) {
 			throw new UnAuthrizedException("인증된 사용자가 아닙니다", HttpStatus.UNAUTHORIZED);
 		}
+		
+		return "account/list";
+	}
+	
+	// http://localhost:80/account/save
+	@GetMapping("/save")
+	public String save() {
+		
+		// 인증 검사
+		User principal = (User)session.getAttribute(Define.PRINCIPAL);
+		if(principal == null) {
+			throw new UnAuthrizedException("로그인 먼저 해주세요", HttpStatus.UNAUTHORIZED);
+		}
+		
+		
+		return "account/save";
+	}
+	
+	
+	@PostMapping("/save")
+	public String saveProc(SaveFormDto dto) {
+		
+		// 1. 인증 검사
+		User principal = (User)session.getAttribute(Define.PRINCIPAL);
+		if(principal == null) {
+			throw new UnAuthrizedException("로그인 먼저 해주세요", HttpStatus.UNAUTHORIZED);
+		}
+		
+		// 2. 유효성 검사
+		if(dto.getNumber() == null || dto.getNumber().isEmpty()) {
+			throw new CustomRestfullException("계좌번호를 입력해주세요", HttpStatus.BAD_REQUEST);
+		}
+		if(dto.getPassword() == null || dto.getPassword().isEmpty()) {
+			throw new CustomRestfullException("계좌 비밀번호를 입력해주세요", HttpStatus.BAD_REQUEST);
+		}
+		if(dto.getBalance() == null || dto.getBalance() <= 0) {
+			throw new CustomRestfullException("잘못된 입력 입니다", HttpStatus.BAD_REQUEST);
+		}
+		
+		// 3. 핵심 로직
+		accountService.createAccount(dto, principal.getId());
 		
 		return "account/list";
 	}
