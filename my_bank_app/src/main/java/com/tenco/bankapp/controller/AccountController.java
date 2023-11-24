f6f9e4d34cc7798403bcf4fc04c578671c47cd72
muaga <more9494@naver.com>
2023-11-24 15:32:04 +0900

    view만 오나횽

--- my_bank_app/src/main/java/com/tenco/bankapp/controller/AccountController.java
+++ my_bank_app/src/main/java/com/tenco/bankapp/controller/AccountController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tenco.bankapp.dto.SaveFormDto;
+import com.tenco.bankapp.dto.WithdrawFormDto;
 import com.tenco.bankapp.handler.exception.CustomPageException;
 import com.tenco.bankapp.handler.exception.CustomRestfullException;
 import com.tenco.bankapp.handler.exception.UnAuthrizedException;
@@ -95,5 +96,49 @@ public class AccountController {
 	}
 	
 	
-
+	// 출금 페이지
+	// http://localhost:80/account/withdraw
+	@GetMapping("/withdraw")
+	public String withdraw() {
+		
+		// 1. 인증 검사
+		User principal = (User)session.getAttribute(Define.PRINCIPAL);
+		if(principal == null) {
+			throw new UnAuthrizedException("로그인 먼저 해주세요", HttpStatus.UNAUTHORIZED);
+		}
+		
+		return "account/withdraw";
+	}
+	
+	@PostMapping("/withdraw")
+	public String withdrawProc(WithdrawFormDto dto) {
+		
+		// 1. 인증 검사
+		User principal = (User)session.getAttribute(Define.PRINCIPAL);
+		if(principal == null) {
+			throw new UnAuthrizedException("로그인 먼저 해주세요", HttpStatus.UNAUTHORIZED);
+		}
+		
+		// 2. 유효성 검사
+		if(dto.getAmount() == null) {
+			throw new CustomRestfullException("금액이 입력해주세요", HttpStatus.BAD_REQUEST);
+		}
+		
+		if(dto.getAmount().longValue() <= 0) {
+			throw new CustomRestfullException("출금 금액이 0원 이하일 수 없습니다", HttpStatus.BAD_REQUEST);
+		}
+		
+		if(dto.getWAccountNumber() == null || dto.getWAccountNumber().isEmpty()) {
+			throw new CustomRestfullException("출금 계좌 번호를 입력해주세요", HttpStatus.BAD_REQUEST);
+		}
+		
+		if(dto.getPassword() == null || dto.getPassword().isEmpty()) {
+			throw new CustomRestfullException("출금 계좌 비밀 번호를 입력해주세요", HttpStatus.BAD_REQUEST);
+		}
+		
+		// 3. 핵심 로직
+		accountService.updateAccountWithdraw(dto, principal.getId());
+		
+		return "redirect:/account/list";
+	}
 }
